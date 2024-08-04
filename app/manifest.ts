03888ef434e9afc82c9a78267221564414508cee
HoDienCong <hodiencong2000@gmail.com>
2024-08-04 12:36:00 +0700

    fix build index

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,44 +2,45 @@ import { MetadataRoute } from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Hồ Diên Công - Software Engineer',
+    name: 'Hồ Diên Công',
     short_name: 'HoDienCong_SoftwareEngineer',
-    description: 'Hồ Diên Công - Software Engineer',
+    description:
+      'I am A Software Engineer. I can build  website, app mobile and API service the best to you',
     start_url: '/',
     display: 'fullscreen',
     background_color: '#fff',
     theme_color: '#fff',
-    "icons": [
+    icons: [
       {
-        "src": "36x36.png",
-        "sizes": "36x36",
-        "type": "image/png",
+        src: '36x36.png',
+        sizes: '36x36',
+        type: 'image/png',
       },
       {
-        "src": "48x48.png",
-        "sizes": "48x48",
-        "type": "image/png",
+        src: '48x48.png',
+        sizes: '48x48',
+        type: 'image/png',
       },
       {
-        "src": "72x72.png",
-        "sizes": "72x72",
-        "type": "image/png",
+        src: '72x72.png',
+        sizes: '72x72',
+        type: 'image/png',
       },
       {
-        "src": "96x96.png",
-        "sizes": "96x96",
-        "type": "image/png",
+        src: '96x96.png',
+        sizes: '96x96',
+        type: 'image/png',
       },
       {
-        "src": "144x144.png",
-        "sizes": "144x144",
-        "type": "image/png",
+        src: '144x144.png',
+        sizes: '144x144',
+        type: 'image/png',
       },
       {
-        "src": "192x192.png",
-        "sizes": "192x192",
-        "type": "image/png",
-      }
+        src: '192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
     ],
   }
 }
