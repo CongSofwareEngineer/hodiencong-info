55ca7535fd0393ba0c8f18555f5c01ba5d3fe3e1
congbacoor <cong@bacoor.co>
2025-12-26 11:38:48 +0700

    feat: Add blog page, new CV PDF, and update CV link in app constants.

--- constants/app.ts
+++ constants/app.ts
@@ -27,7 +27,7 @@ export enum LINK_CONTACT {
   Github = 'https://github.com/CongSofwareEngineer',
   SDT = 'tel:+84392225405',
   Mail = 'mailto:hodiencong2000@gmail.com',
-  CV = 'https://www.topcv.vn/xem-cv/UFNWCwJVXldaBg5XVAQPDAQBAQxVD1RRAAUAAAedeb',
+  CV = 'https://hdcong.vercel.app/Ho-Dien-Cong-MiddleReact-CV.pdf',
   GGMap = 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d245.27946055157543!2d104.47439031761917!3d10.38408607257531!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3109db6752fd6d67%3A0xfdc817d347739de1!2zTmjDoCB0cuG7jSBiw6xuaCBkw6JuIFRoYW5oIER1eQ!5e0!3m2!1sen!2s!4v1750173449168!5m2!1sen!2s',
 }
 
