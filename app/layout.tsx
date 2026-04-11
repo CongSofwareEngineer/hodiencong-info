d26674b08443d8d7d3e9575b7f8d388a4d5f4f48
HoDienCong <hodiencong2000@gmail.com>
2026-04-11 10:42:28 +0700

    fix googletagmanager

--- app/layout.tsx
+++ app/layout.tsx
@@ -103,13 +103,29 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {/* {process.env.NEXT_PUBLIC_MODE_PRODUCTION && <GoogleTagManager gtmId='GTM-5V3MQKJS' />} */}
         {process.env.NEXT_PUBLIC_MODE_PRODUCTION && (
           <>
-            <Script id='gtm-script' strategy='afterInteractive'>
+            <Script id='gtm-script' strategy='beforeInteractive'>
               {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-5V3MQKJS');`}
             </Script>
+          </>
+        )}
+
+        {process.env.NEXT_PUBLIC_MODE_PRODUCTION && (
+          <>
+            {/* <GoogleAnalytics gaId='GTM-5V3MQKJS' /> */}
+            <>
+              <Script async src='https://www.googletagmanager.com/gtag/js?id=G-33FPFZ43ND' />
+              <Script id='gtm-script' strategy='beforeInteractive'>
+                {`  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-33FPFZ43ND');`}
+              </Script>
+            </>
           </>
         )}
 
@@ -142,21 +158,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           </>
         )}
       </head>
-      {process.env.NEXT_PUBLIC_MODE_PRODUCTION && (
-        <>
-          {/* <GoogleAnalytics gaId='GTM-5V3MQKJS' /> */}
-          <>
-            <Script async src='https://www.googletagmanager.com/gtag/js?id=G-33FPFZ43ND' />
-            <Script id='gtm-script' strategy='afterInteractive'>
-              {`  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-33FPFZ43ND');`}
-            </Script>
-          </>
-        </>
-      )}
       <body
         className={clsx(
           inter.variable,
