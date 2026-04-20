ebc7e4be49d20959890435f3d2ab8cc0ff9f7d7e
congbacoor <cong@bacoor.co>
2026-04-20 14:11:57 +0700

    fix ui

--- app/laundry/page.tsx
+++ app/laundry/page.tsx
@@ -9,7 +9,7 @@ export async function generateMetadata(_: any, parent: ResolvingMetadata): Promi
   const metaData = generateMetaBase({
     dataBase: preData,
     override: true,
-    title: 'Giặt Ủi Sinh Viên Thủ Dầu Một - Giá Rẻ, Sạch Thơm | Ho Dien Cong',
+    title: 'Giặt Ủi Sinh Viên - Giá Rẻ, Sạch Thơm | Ho Dien Cong',
     des: 'Dịch vụ giặt ủi chuyên nghiệp dành riêng cho sinh viên Thủ Dầu Một. Giá cực rẻ, giặt riêng sạch sẽ, lấy nhanh trong ngày. Uy tín, minh bạch, giúp bạn tiết kiệm thời gian và chi phí.',
   })
 
