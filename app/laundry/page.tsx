ac7e0c89447e37c08f24e32aa13ad82c7bc8b8de
congbacoor <cong@bacoor.co>
2026-04-20 14:10:45 +0700

    fix SEO

--- app/laundry/page.tsx
+++ app/laundry/page.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import { Metadata } from 'next'
+import { Metadata, ResolvingMetadata } from 'next'
 
 import LaundryClient from './client'
 
-export const metadata: Metadata = {
-  title: 'Dịch vụ giặt ủi - Ho Dien Cong',
-  description:
-    'Bạn đang tìm dịch vụ giặt ủi giá rẻ nhưng vẫn đảm bảo chất lượng? Đừng bỏ qua dịch vụ giặt ủi chuyên nghiệp của chúng tôi. Giá thành hợp lý, minh bạch, phù hợp cho mọi cá nhân và gia đình.',
+import { generateMetaBase } from '@/utils/serverNext'
+
+export async function generateMetadata(_: any, parent: ResolvingMetadata): Promise<Metadata> {
+  const preData = await parent
+  const metaData = generateMetaBase({
+    dataBase: preData,
+    override: true,
+    title: 'Giặt Ủi Chuyên Nghiệp, Giá Rẻ & Uy Tín | Ho Dien Cong',
+    des: 'Bạn đang tìm dịch vụ giặt ủi giá rẻ nhưng vẫn đảm bảo chất lượng? Đừng bỏ qua dịch vụ giặt ủi chuyên nghiệp của chúng tôi. Giá thành hợp lý, minh bạch, phù hợp cho mọi cá nhân và gia đình.',
+  })
+
+  return metaData
 }
 
 export default function LaundryPage() {
