6fc42edda19fb643e4d44003f76519961aee149b
congbacoor <cong@bacoor.co>
2026-04-20 14:11:32 +0700

    fix SEO

--- app/laundry/page.tsx
+++ app/laundry/page.tsx
@@ -9,8 +9,8 @@ export async function generateMetadata(_: any, parent: ResolvingMetadata): Promi
   const metaData = generateMetaBase({
     dataBase: preData,
     override: true,
-    title: 'Giặt Ủi Chuyên Nghiệp, Giá Rẻ & Uy Tín | Ho Dien Cong',
-    des: 'Bạn đang tìm dịch vụ giặt ủi giá rẻ nhưng vẫn đảm bảo chất lượng? Đừng bỏ qua dịch vụ giặt ủi chuyên nghiệp của chúng tôi. Giá thành hợp lý, minh bạch, phù hợp cho mọi cá nhân và gia đình.',
+    title: 'Giặt Ủi Sinh Viên Thủ Dầu Một - Giá Rẻ, Sạch Thơm | Ho Dien Cong',
+    des: 'Dịch vụ giặt ủi chuyên nghiệp dành riêng cho sinh viên Thủ Dầu Một. Giá cực rẻ, giặt riêng sạch sẽ, lấy nhanh trong ngày. Uy tín, minh bạch, giúp bạn tiết kiệm thời gian và chi phí.',
   })
 
   return metaData
