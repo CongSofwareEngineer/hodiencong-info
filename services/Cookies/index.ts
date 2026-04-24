001e885920b5f72ac6e7716efd18be51fe7865cc
congbacoor <cong@bacoor.co>
2026-04-24 10:33:39 +0700

    fix setCookie

--- services/Cookies/index.ts
+++ services/Cookies/index.ts
@@ -33,7 +33,7 @@ export async function setCookie(key: COOKIE_KEY, value: any, expired?: number) {
     cookieStore.set(key, value, {
       maxAge: expired,
       httpOnly: true,
-      sameSite: 'strict',
+      sameSite: 'lax',
       secure: true,
       path: '/',
     })
