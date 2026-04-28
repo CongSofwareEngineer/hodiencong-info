dd029e8593500060a83e0445a07fa995e9f01664
HoDienCong <hodiencong2000@gmail.com>
2026-04-28 19:31:12 +0700

    fix SEO

--- config/site.ts
+++ config/site.ts
@@ -3,6 +3,17 @@ export const SITE_CONFIG = {
   description: 'I am A Software Engineer. I can build websites, app mobile and API service that best to you',
   images: '/favicon.ico',
   icon: '/favicon.ico',
-  keywords: ['Hồ Diên Công', 'Công', 'Software Engineer', 'Công front end', 'Công full stack', 'Reactjs', 'React native', 'Website'],
+  keywords: [
+    'cong',
+    'Hồ Diên Công',
+    'Công',
+    'ho dien cong',
+    'Software Engineer',
+    'Công front end',
+    'Công full stack',
+    'Reactjs',
+    'React native',
+    'Website',
+  ],
   url: 'https://hdcong.vercel.app/',
 }
