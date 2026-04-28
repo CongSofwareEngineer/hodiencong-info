afdcdb54272b07451ea44167a26b779a1062cf76
HoDienCong <hodiencong2000@gmail.com>
2026-04-28 19:37:30 +0700

    fix seo

--- config/site.ts
+++ config/site.ts
@@ -1,6 +1,7 @@
 export const SITE_CONFIG = {
-  title: 'Hồ Diên Công',
-  description: 'I am A Software Engineer. I can build websites, app mobile and API service that best to you',
+  title: 'Hồ Diên Công | Fullstack Software Engineer',
+  description:
+    'Professional Software Engineer dedicated to building high-performance websites, mobile applications, and robust API services tailored to your needs.',
   images: '/favicon.ico',
   icon: '/favicon.ico',
   keywords: [
